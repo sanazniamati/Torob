1cbef54300e89d0234d1ae11d335aebb1f48fcbf
sanaz-niamati <sanaz.niamati@yahoo.com>
2022-02-01 19:45:42 +0330

    start trob project

---
+++ src/componet/Home/Home .js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Home.css";
+import torobLogo from "../assets/images/torob_logo.svg";
+import search from "../assets/images/search.svg";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <header></header>
+      <main>
+        <div className="logo-wrapper">
+          <div className="logo">
+            <div>
+              <a href="/">
+                <img src={torobLogo} alt="torobLogo" />
+              </a>
+            </div>
+          </div>
+          <h1 className="logo-text">
+            ترب
+            <p className="slogan">موتور جستجوی هوشمند خرید</p>
+          </h1>
+        </div>
+        <div className="search">
+          <form id="searchFormInput" className="search-form-home">
+            <div className="search-btn-home">
+              <img
+                src={search}
+                style={{ width: 46, height: 46 }}
+                alt="search"
+              />
+            </div>
+            {/* <input
+              type="text"
+              className="search-text-home"
+              placeholder="نام کالا را وارد کنید"
+            /> */}
+          </form>
+        </div>
+        <div className="offers-btn">
+          <a className="link-on-click offers-btn-txt" href="/">
+            تخفیف&zwnj;ها
+          </a>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
